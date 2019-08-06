941a7c19aa7e186f7562d411252c99850e6d83f0
Abheet Gera <abheet.gera01@gmail.com>
2019-08-06 19:49:05 +0530

    Update SumOfSubarrays.cpp

--- SumOfSubarrays.cpp
+++ SumOfSubarrays.cpp
@@ -4,20 +4,17 @@ using namespace std;
 int main() {
 	// your code goes here
 	vector<int> a;
-	int s,sum=0;
+	int s=0;
 	a.push_back(1);
 	a.push_back(2);
 	a.push_back(3);
 	a.push_back(4);
+	int l=1,r=a.size();
 	for(int i=0;i<a.size();i++){
-	    for(int j=i;j<a.size();j++){
-	        s=0;
-	        for(int k=i;k<=j;k++){
-	            s+=a[k];
-	        }
-	        sum+=s;
-	    }
+	    s+=a[i]*l*r;
+	    l++;
+	    r--;
 	}
-	cout<<sum;
+	cout<<s;
 	return 0;
 }
